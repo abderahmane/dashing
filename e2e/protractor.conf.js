7cfbb40daaa15d6a8a877f3839c2c2573f1901c0
mac <abdasidi@gmail.com>
2019-11-08 09:04:48 +0000

    feat(ep3): add CI part1
    add config chrome(protractor & karma)

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -13,7 +13,10 @@ exports.config = {
     './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    browserName: 'chrome'
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--headless', '--no-sandbox', '--disable-gpu', '--window-size=1280,1024']
+    }
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
